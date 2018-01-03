c5079e5be5244444848593638c77544b2a590326
Ting <ticccccc@163.com>
2018-01-03 17:40:39 +0800

    使用AndroidExtensions代替ButterKnife

--- User/src/main/java/com/work/user/ui/activity/RegisterActivity.kt
+++ User/src/main/java/com/work/user/ui/activity/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.work.user.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.user.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
