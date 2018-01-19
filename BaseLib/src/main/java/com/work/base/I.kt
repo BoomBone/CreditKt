28070f5ffc1251fa66fba4a28a4c3a1feadc7e26
Ting <ticccccc@163.com>
2018-01-19 15:48:39 +0800

    添加我自己写的retrofit-rx封装

---
+++ BaseLib/src/main/java/com/work/base/I.kt
@@ -0,0 +1,14 @@
+package com.work.base
+
+/**
+ * @author Ting
+ * @date 2017/11/10
+ */
+
+interface I {
+    interface BaseUrl {
+        companion object {
+            val URL = "http://news.baidu.com/"
+        }
+    }
+}
--- User/src/main/java/com/work/user/ui/activity/RegisterActivity.kt
+++ User/src/main/java/com/work/user/ui/activity/RegisterActivity.kt
@@ -2,16 +2,31 @@ package com.work.user.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.work.base.ui.activity.BaseMvpActivity
+import com.work.ddnet.rx.RxRestClient
 import com.work.user.R
 import com.work.user.presenter.RegisterPresenter
 import com.work.user.presenter.view.RegisterView
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.annotations.NonNull
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.toast
 
 class RegisterActivity : BaseMvpActivity<RegisterPresenter>(), RegisterView {
     override fun onRegisterResult(result: Boolean) {
         toast("注册成功")
+        RxRestClient.builder()
+                .url("http://news.baidu.com/")
+                .build()
+                .get()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(Consumer<String> { s -> toast(s) })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +37,7 @@ class RegisterActivity : BaseMvpActivity<RegisterPresenter>(), RegisterView {
         mPresenter.mView = this
 
         btn_register.setOnClickListener {
-            mPresenter.register("", "","")
+            mPresenter.register("", "", "")
         }
     }
 }
