89155f992fae01f92aea73f5fb9cf3fd4197dca3
Ting <ticccccc@163.com>
2018-01-24 10:34:51 +0800

    ProgressLoading封装

--- BaseLib/src/main/java/com/work/base/ui/activity/BaseMvpActivity.kt
+++ BaseLib/src/main/java/com/work/base/ui/activity/BaseMvpActivity.kt
@@ -1,20 +1,35 @@
 package com.work.base.ui.activity
 
+import android.os.Bundle
 import com.work.base.presenter.BasePresenter
 import com.work.base.presenter.view.BaseView
+import com.work.base.widgets.ProgressLoading
+import org.jetbrains.anko.toast
 
 /**
  * Created by Ting on 2018/1/4.
  */
-open class BaseMvpActivity<T : BasePresenter<*>> : BaseActivity(),BaseView {
+abstract open class BaseMvpActivity<T : BasePresenter<*>> : BaseActivity(), BaseView {
+
+    private lateinit var mLoadingDialog: ProgressLoading
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //初始化加载框
+        mLoadingDialog = ProgressLoading.create(this)
+    }
+
     override fun showLoading() {
+        mLoadingDialog.showLoading()
     }
 
     override fun hideLoading() {
+        mLoadingDialog.hideLoading()
     }
 
-    override fun onError() {
+    override fun onError(text: String) {
+        toast(text)
     }
 
-    lateinit var mPresenter:T
+    lateinit var mPresenter: T
 }
