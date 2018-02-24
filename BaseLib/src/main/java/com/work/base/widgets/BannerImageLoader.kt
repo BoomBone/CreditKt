637183278e77af1b2324a5d18ee3e734150c766c
Ting <ticccccc@163.com>
2018-02-24 13:43:49 +0800

    导入banner依赖和首页常量
    (cherry picked from commit a639385)

---
+++ BaseLib/src/main/java/com/work/base/widgets/BannerImageLoader.kt
@@ -0,0 +1,16 @@
+package com.work.base.widgets
+
+import android.content.Context
+import android.widget.ImageView
+import com.work.base.utils.GlideUtils
+import com.youth.banner.loader.ImageLoader
+
+/**
+ * Created by Ting on 2018/2/24.
+ * banner图片加载器
+ */
+class BannerImageLoader : ImageLoader() {
+    override fun displayImage(context: Context, path: Any, imageView: ImageView) {
+        GlideUtils.loadUrlImage(context, path.toString(), imageView)
+    }
+}
