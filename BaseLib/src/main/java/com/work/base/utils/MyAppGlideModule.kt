dc9fc98e420043e3e54a83546b454bff8debc651
Ting <ticccccc@163.com>
2018-02-28 14:38:08 +0800

    更换glide版本

---
+++ BaseLib/src/main/java/com/work/base/utils/MyAppGlideModule.kt
@@ -0,0 +1,10 @@
+package com.work.base.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by Ting on 2018/2/28.
+ */
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
